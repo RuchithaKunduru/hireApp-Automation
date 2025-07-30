3854c66b0b06787eb4574cd69325a970469eb906
Ruchitha Kunduru <ruchitha.sai@techolution.com>
2025-07-30 15:04:10 +0530

    changes added

---
+++ HireApp/cucumber.js
@@ -0,0 +1,10 @@
+module.exports = {
+  default: [
+    '--require-module ts-node/register',
+    '--require test/steps/**/*.ts',
+    '--require test/support/**/*.ts',
+    '--format json:test/test-results/report.json', 
+    // '--publish-quiet',
+    'test/features/**/*.feature'
+  ].join(' ')
+};
