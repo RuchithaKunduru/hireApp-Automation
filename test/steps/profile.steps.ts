152dc05d123882a743e691e9ecbc4b103c07f516
PavanPenumajji <pavan.penumajji@techolution.com>
2025-11-10 18:49:46 +0530

    Remove profile steps from Cucumber tests

--- test/steps/profile.steps.ts
+++ test/steps/profile.steps.ts
@@ -1,18 +1,1 @@
-import { Given, When, Then } from "@cucumber/cucumber";
-import { Page, BrowserContext } from "playwright";
-import { ProfilePage } from "../pages/profilePage";
-
-let page: Page;
-let context: BrowserContext;
-let profilePage: ProfilePage;
-
-Given("user clicks on the profile button", async function(){
-    const profilePage = new ProfilePage(this.page!);
-    await profilePage.clickProfileButton();
-});
-
-When("user should be able to see the title {string}",async function(profileName:string){
-    const profilePage = new ProfilePage(this.page!);
-    await profilePage.verifyProfileName(profileName);
-});
 
